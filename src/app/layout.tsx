57d102926d0026348981b233ccdd6202acdde2df
AmirZalaghi <zalaghi.dev@gmail.com>
2026-08-28 16:24:40 +0330

    feat: font & components

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,8 @@
 import type { Metadata } from "next";
-import { Geist_Mono, Noto_Sans_Arabic } from "next/font/google";
+import { Geist_Mono } from "next/font/google";
 import { DirectionProvider } from "@/components/ui/direction";
+import "@/styles/fonts-iran-yekan.css";
 import "./globals.css";
-
-const fontSans = Noto_Sans_Arabic({
-  subsets: ["arabic"],
-  variable: "--font-noto-sans",
-});
 
 const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
@@ -23,7 +19,7 @@ export default function RootLayout({ children }: LayoutProps<"/">) {
     <html
       lang="fa"
       dir="rtl"
-      className={`${fontSans.variable} ${geistMono.variable} h-full antialiased`}
+      className={`${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
         <DirectionProvider direction="rtl">{children}</DirectionProvider>
